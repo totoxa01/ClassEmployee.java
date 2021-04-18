b1f846d45e0b7d5f7b55a0c6641444a300eac4e0
glowwormsv <adm_kot@mail.ru>
2021-04-18 13:44:59 +0300

    Практическая работа № 13

---
+++ src/main/java/ru/geekbrains/pr13/Tunnel.java
@@ -0,0 +1,33 @@
+package ru.geekbrains.pr13;
+
+import java.util.concurrent.Semaphore;
+
+import static ru.geekbrains.pr13.ClassWork.HALF_CARS_COUNT;
+
+public class Tunnel extends Stage{
+    Semaphore smp = new Semaphore(HALF_CARS_COUNT);
+
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+
+    @Override
+    public void go(Car c) {
+        try {
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                smp.acquire();
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+                smp.release();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
